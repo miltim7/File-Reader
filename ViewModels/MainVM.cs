66911afc880ddd0d5b6d3a4e0b1a128a6ed11cd3
miltim7 <timursailing70@gmail.com>
2023-07-07 01:33:40 +0400

    add continue button

--- ViewModels/MainVM.cs
+++ ViewModels/MainVM.cs
@@ -6,23 +6,22 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.IO;
+using System.Printing;
 using System.Security.Principal;
+using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Threading;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace FileReader.ViewModels;
 
 public class MainVM : BaseVM
 {
     CancellationTokenSource tokenSource;
-
-    int symbol;
-    int word;
-    int sentence;
-
+    bool makeReset = true;
     public MainVM()
     {
         AddFiles();
@@ -54,6 +53,21 @@ public class MainVM : BaseVM
     {
         get => selectedItem;
         set => OnPropertyChanged(out selectedItem, value);
+    }
+
+    private string contentName;
+    public string ContentName
+    {
+        get => contentName;
+        set => OnPropertyChanged(out contentName, value);
+    }
+
+    private string content;
+    public string Content
+
+    {
+        get => content;
+        set => OnPropertyChanged(out content, value);
     }
 
 
@@ -97,9 +111,23 @@ public class MainVM : BaseVM
 
     private void DoAnalize()
     {
+        if(makeReset)
+        {
+            ProgressBar = 0;
+            Sentences = 0;
+            Words = 0;
+            SymbolCount = 0;
+            Content = string.Empty;
+            ContentName = string.Empty;
+        }
+
+        makeReset = true;
+        tokenSource = new CancellationTokenSource();
+        double progress = GetProgress();
+        StreamReader reader = new StreamReader(SelectedItem.File);
+        ContentName = SelectedItem.File;
         var task = new Task(() =>
         {
-            StreamReader reader = new StreamReader(SelectedItem.File);
             while(!reader.EndOfStream)
             {
                 if(tokenSource.IsCancellationRequested)
@@ -112,26 +140,31 @@ public class MainVM : BaseVM
 
                 char c = (char) nextChar;
 
-                if(c == ' ')
-                    word++;
-
-                if(c == '.')
-                    sentence++;
-
-                symbol++;
-
                 Console.Write(c);
+
+                Content += c;
 
                 Thread.Sleep(10);
 
-                ProgressBar += 0.01;
+                ProgressBar += progress;
             }
         });
 
         task.Start();
     }
 
+    private double GetProgress() => 100 / (double)File.ReadAllText(SelectedItem.File).Length;
 
+    private void SetStats(string content)
+    {
+        SymbolCount = content.Length;
+        Words = content.Split(new char[] { ' ', '\t', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries).Length;
+        
+        Regex regex = new Regex(@"\b[^.!?]*[.!?](?=\s|$)");
+        MatchCollection matches = regex.Matches(content);
+
+        Sentences = matches.Count;
+    }
 
     private RelayCommand cancel;
     public RelayCommand Cancel
@@ -141,10 +174,20 @@ public class MainVM : BaseVM
     }
     private void DoCancel()
     {
-        SymbolCount = symbol;
-        Sentences = sentence;
-        Words = word;
-
+        SetStats(Content);
         tokenSource.Cancel();
+    }
+
+    private RelayCommand continueCommand;
+    public RelayCommand Continue
+    {
+        get => continueCommand ??= new RelayCommand(DoContinue);
+        set => OnPropertyChanged(out continueCommand, value);
+    }
+    private void DoContinue()
+    {
+        SetStats(Content);
+        makeReset = false;
+        DoAnalize();
     }
 }
